1a6cacc3c47c1efa0a587f8eab1eb0bac8ca0a45
Ricardo <ricardo.mr.alves@gmail.com>
2024-02-14 13:06:20 +0000

    a

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionFormComponent } from './transactions/transaction-form/transaction-form.component';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+  { path: '', component: DashboardComponent }, // Dashboard as default route
+  { path: 'transactions/new', component: TransactionFormComponent } // Route for transaction form
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
