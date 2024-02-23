4ff24a23afe5e9fa83a20176b0e94bac1cbd0988
Ricardo <ricardo.mr.alves@gmail.com>
2024-02-23 00:30:20 +0000

    Some small style changes and pie chart

--- client/src/app/spending-pie-chart/spending-pie-chart.component.ts
+++ client/src/app/spending-pie-chart/spending-pie-chart.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ChartData, ChartType } from 'chart.js';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart, ChartData, ChartOptions, ChartType } from 'chart.js';
 import { NgChartsModule } from 'ng2-charts';
 import { TransactionService } from '../transaction.service';
 import { SpendingByCategory } from '../spending-by-category';
@@ -11,10 +11,32 @@ import { SpendingByCategory } from '../spending-by-category';
   templateUrl: './spending-pie-chart.component.html',
   styleUrl: './spending-pie-chart.component.css'
 })
-export class SpendingPieChartComponent {
-  public pieChartOptions = {
+export class SpendingPieChartComponent implements AfterViewInit{
+
+  @ViewChild('pieChartCanvas') pieChartCanvas!: ElementRef<HTMLCanvasElement>;
+
+  public pieChartOptions: ChartOptions = {
     responsive: true,
+    plugins: {
+      legend: {
+        display: true, // Control the display of the legend
+        position: 'right', // Adjust legend position if necessary
+        labels: {
+          font: {
+            size: 10, // Adjust font size as needed
+          },
+        },
+      },
+      tooltip: {
+        bodyFont: {
+          size: 10, // Adjust tooltip font size
+        },
+        callbacks: {
+        }
+      },
+    },
   };
+  
   public pieChartLabels: string[] = [];
   public pieChartData: ChartData<'pie', number[], string | string[]> = {
     labels: this.pieChartLabels,
@@ -42,5 +64,34 @@ export class SpendingPieChartComponent {
         }]
       };
     });
+  }
+
+  ngAfterViewInit() {
+    this.scaleCanvasForHighDPI();
+  }
+
+  scaleCanvasForHighDPI() {
+    const canvas: HTMLCanvasElement = this.pieChartCanvas.nativeElement;
+    const ctx = canvas.getContext('2d');
+
+        // Check if the context is null
+    if (!ctx) {
+      console.error('Failed to get 2D context');
+      return; // Early return if ctx is null
+    }
+
+    // Assume the desired width and height are 300x300 (to match your div container)
+    let desiredWidth = 300;
+    let desiredHeight = 300;
+
+    // Adjust for device pixel ratio
+    let devicePixelRatio = window.devicePixelRatio || 1;
+    canvas.width = desiredWidth * devicePixelRatio;
+    canvas.height = desiredHeight * devicePixelRatio;
+    canvas.style.width = `${desiredWidth}px`;
+    canvas.style.height = `${desiredHeight}px`;
+
+    // Scale the context to ensure correct drawing dimensions
+    ctx.scale(devicePixelRatio, devicePixelRatio);
   }
 }
